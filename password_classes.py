9efe1e975e8ec389aad1d63277d614b60e287849
Emereole Chinduji <60651716+LowUp@users.noreply.github.com>
2023-12-20 12:05:06 +0100

    Code improvements
    Program structure change to make the code more readable

---
+++ password_classes.py
@@ -0,0 +1,124 @@
+import mysql.connector
+from mysql.connector import Error
+from dataclasses import dataclass
+
+@dataclass(frozen=True)
+class Query_manager:
+    connection: mysql
+    
+    def __post_init_(self) -> None:
+        # self.connection = mysql.connector.connect(host = self.host, database = self.database, user = self.user,password = self.password)
+        if self.connection.is_connected():
+            db_Info = self.connection.get_server_info()
+            print("Connected to MySQL Server version ", db_Info)
+        else:
+            print("Error while connecting to MySQL", Error)
+    
+    def display_all_passwords(self) -> None:
+        cursor = self.connection.cursor()
+        cursor.execute("select * from passwords;")
+        record = cursor.fetchall()
+        print("| id | - | service |")
+        for count, value in enumerate(record):
+            print(
+                f"| {value[0]} | - | {value[1]} |\n")
+        cursor.close()
+    
+    def display_password(self, id) -> None:
+        cursor = self.connection.cursor()
+        cursor.execute(f"select * from passwords where id = {id};")
+        record = cursor.fetchall()
+        
+        if len(record) == 0:
+            print("No password found")
+            return None
+        else:
+            print("| id | - | service | - | username | - | password |\n")
+            for count, value in enumerate(record):
+                print(
+                    f"| {value[0]} | - | {value[1]} | - | {value[2]} | - | {value[3]} |\n")
+                return None
+        cursor.close()
+    
+    def add_password(self) -> None:
+        cursor = self.connection.cursor()
+        service = input("Input service name : \t")
+        identifiant = input("Input username : \t")
+        mdp = input("Type your password : \t")
+        cursor.execute(
+            f"insert into passwords (service, identifiant, mdp) values ('{service}', '{identifiant}', '{mdp}');")
+        self.connection.commit()
+        print("Password added")
+        cursor.close()
+    
+    def edit_password(self, id) -> None:
+        cursor = self.connection.cursor()
+        cursor.execute(f"select * from passwords where id = {id};")
+        record = cursor.fetchall()
+        input_dict = {}
+        
+        if len(record) == 0:
+            print("No password found")
+            return None
+        
+        for count, value in enumerate(record):
+            print("| id | - | service | - | username | - | password |\n")
+            print(
+            f"| {value[0]} | - | {value[1]} | - | {value[2]} | - | {value[3]} |\n")
+        
+        print("Leave blank and press ENTER to not modify values\n")
+        service_input = input("Update service name : \t")
+        input_dict.update({"service": service_input})
+        
+        identifiant_input = input("Update identifiant : \t")
+        input_dict.update({"identifiant": identifiant_input})
+
+        mdp_input = input("Update password : \t")
+        input_dict.update({"mdp": mdp_input})
+
+        sql_query = "update passwords set "
+        counter = 0 
+
+        for key, value in input_dict.items():
+            if str(value).lower() != '':
+                if counter == 0:
+                    sql_query+=f"{key} = '{value}' "
+                else:
+                    sql_query+=f", {key} = '{value}' "
+                counter += 1
+        
+        sql_query+=f"where id = {id}"
+        
+        if sql_query != f"update passwords set where id = {id}":
+            cursor.execute(
+                    sql_query)
+            self.connection.commit()
+            print("Record updated !")
+        else:
+            print("\nNo changes made")   
+        cursor.close()
+        
+    def delete_password(self, id) -> None:
+        cursor = self.connection.cursor()
+        cursor.execute(f"select * from passwords where id = {id};")
+        record = cursor.fetchall()
+        
+        if len(record) == 0:
+            print("No password found")
+            return None
+        
+        cursor.execute(
+            f"delete from passwords where id = {id} ")
+        self.connection.commit()
+        print("Record deleted !")
+        cursor.close()
+    
+    def close_connection(self) -> None:
+        if self.connection.is_connected():
+            self.connection.close()
+            print("MySQL connection is closed")
+        else:
+            print("Error while closing connection")
+        
+    
+    
--- password_container.py
+++ password_container.py
@@ -2,6 +2,7 @@ import mysql.connector
 from mysql.connector import Error
 from dotenv import dotenv_values
 import getpass
+from password_classes import Query_manager
 
 config = dotenv_values(".env")
 
@@ -9,123 +10,50 @@ config = dotenv_values(".env")
 try:
     masterPass = getpass.getpass("Master password: \t")
     if masterPass != config['MASTER_KEY']:
+        connection = None
         raise Exception("Access denied !")
     else :
+        
         connection = mysql.connector.connect(host=config['HOST'],
                                             database=config['BDD'],
                                             user=config['USER'],
                                             password=config['PASSWORD'])
-        if connection.is_connected():
-            db_Info = connection.get_server_info()
-            print("Connected to MySQL Server version ", db_Info)
+        
+        bdd_connection = Query_manager(connection)   
+        
+        print("Welcome to your password manager !")
 
-            while True:
-                user_input = input(
-                    "\n*****************************************\n\t1 - List services passwords\n\t2 - Add a password\n\t3 - Update a record\n\t4 - Delete a record\n\tAny other key - exit\n*****************************************\n")
-                cursor = connection.cursor()
-                if user_input == '1':
-                    cursor.execute("select * from passwords;")
-                    record = cursor.fetchall()
-                    print("| id | - | service |")
-                    for count, value in enumerate(record):
-                        print(
-                            f"| {value[0]} | - | {value[1]} |\n")
-                    
-                    passId = input("Choose password to reveal : \t")
-                    for count, value in enumerate(record):
-                        if value[0] == int(passId):
-                            print("| id | - | service | - | username | - | password |\n")
-                            print(
-                            f"| {value[0]} | - | {value[1]} | - | {value[2]} | - | {value[3]} |\n")
-                            break
+        while True:
+            user_input = input(
+                "\n*****************************************\n\t1 - List services passwords\n\t2 - Add a password\n\t3 - Update a record\n\t4 - Delete a record\n\tAny other key - exit\n*****************************************\n")
+            cursor = connection.cursor()
+            if user_input == '1':
+                bdd_connection.display_all_passwords()
+                
+                passId = input("Choose password to reveal : \t")
+                bdd_connection.display_password(passId)
 
+            elif user_input == '2':     
+                bdd_connection.add_password()
 
-                elif user_input == '2':
-                    service = input("Input service name : \t")
-                    identifiant = input("Input username : \t")
-                    mdp = input("Type your password : \t")
-                    cursor.execute(
-                        f"insert into passwords (service, identifiant, mdp) values ('{service}', '{identifiant}', '{mdp}');")
-                    connection.commit()
-                    print(cursor.rowcount,
-                        "Data inserted !")
-                    cursor.close()
+            elif user_input == '3':
+                bdd_connection.display_all_passwords()
+                
+                id_input = int(input("Which one do you want to update ? \t"))               
+                bdd_connection.edit_password(id_input)
 
-                elif user_input == '3':
-                    input_dict = {}
-                    
-                    cursor.execute("select * from passwords;")
-                    record = cursor.fetchall()
-                    
-                    print("| id | - | service |")
-                    
-                    for count, value in enumerate(record):
-                        print(
-                            f"| {value[0]} | - | {value[1]} |\n")
-                    
-                    id_input = int(input("Which one do you want to update ? \t"))
-                    
-                    for count, value in enumerate(record):
-                        if value[0] == int(id_input):
-                            print("| id | - | service | - | username | - | password |\n")
-                            print(
-                            f"| {value[0]} | - | {value[1]} | - | {value[2]} | - | {value[3]} |\n")
-                            break
-                    
-                    print("Type 'no' & press ENTER to not modify values\n")
-                    
-                    service_input = input("Update service name : \t")
-                    input_dict.update({"service": service_input})
-                    
-                    identifiant_input = input("Update identifiant : \t")
-                    input_dict.update({"identifiant": identifiant_input})
+            elif user_input == '4':
+                bdd_connection.display_all_passwords()
+                
+                id_input = int(input("Which record do you want to delete ? \t"))
+                bdd_connection.delete_password(id_input)
 
-                    mdp_input = input("Update password : \t")
-                    input_dict.update({"mdp": mdp_input})
-
-                    sql_query = "update passwords set "
-                    counter = 0 
-
-                    for key, value in input_dict.items():
-                        if str(value).lower() != 'no':
-                            if counter == 0:
-                                sql_query+=f"{key} = '{value}' "
-                            else:
-                                sql_query+=f", {key} = '{value}' "
-                            counter += 1
-                    
-                    sql_query+=f"where id = {id_input}"
-                 
-                    if sql_query != f"update passwords set where id = {id_input}":
-                        cursor.execute(
-                                sql_query)
-                        connection.commit()
-                        print("Record updated !")
-                        cursor.close()
-
-                elif user_input == '4':
-                    cursor.execute("select * from passwords;")
-                    record = cursor.fetchall()
-                    print("| id | - | service |")
-                    for count, value in enumerate(record):
-                        print(
-                            f"| {value[0]} | - | {value[1]} |\n")
-                    id_input = int(
-                        input("Which record do you want to delete ? \t"))
-                    cursor.execute(
-                        f"delete from passwords where id = {id_input} ")
-                    connection.commit()
-                    print("Record deleted !")
-                    cursor.close()
-
-                else:
-                    break
+            else:
+                break
 
 except Error as e:
     print("Error while connecting to MySQL", e)
     
 finally:
-    if connection.is_connected():
-        cursor.close()
-        connection.close()
-        print("MySQL connection is closed")
+    if connection != None:
+        bdd_connection.close_connection()
