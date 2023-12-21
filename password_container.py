165756e85247bd49d4f10b52dd30030c7223a7d7
Emereole Chinduji <60651716+LowUp@users.noreply.github.com>
2023-12-21 12:09:57 +0100

    main keyword added

--- password_container.py
+++ password_container.py
@@ -6,54 +6,57 @@ from password_classes import Query_manager
 
 config = dotenv_values(".env")
 
+def main():
+    try:
+        masterPass = getpass.getpass("Master password: \t")
+        if masterPass != config['MASTER_KEY']:
+            connection = None
+            raise Exception("Access denied !")
+        else :
+            
+            connection = mysql.connector.connect(host=config['HOST'],
+                                                database=config['BDD'],
+                                                user=config['USER'],
+                                                password=config['PASSWORD'])
+            
+            bdd_connection = Query_manager(connection)   
+            
+            print("Welcome to your password manager !")
 
-try:
-    masterPass = getpass.getpass("Master password: \t")
-    if masterPass != config['MASTER_KEY']:
-        connection = None
-        raise Exception("Access denied !")
-    else :
+            while True:
+                user_input = input(
+                    "\n*****************************************\n\t1 - List services passwords\n\t2 - Add a password\n\t3 - Update a record\n\t4 - Delete a record\n\tAny other key - exit\n*****************************************\n")
+                cursor = connection.cursor()
+                if user_input == '1':
+                    bdd_connection.display_all_passwords()
+                    
+                    passId = input("Choose password to reveal : \t")
+                    bdd_connection.display_password(passId)
+
+                elif user_input == '2':     
+                    bdd_connection.add_password()
+
+                elif user_input == '3':
+                    bdd_connection.display_all_passwords()
+                    
+                    id_input = int(input("Which one do you want to update ? \t"))               
+                    bdd_connection.edit_password(id_input)
+
+                elif user_input == '4':
+                    bdd_connection.display_all_passwords()
+                    
+                    id_input = int(input("Which record do you want to delete ? \t"))
+                    bdd_connection.delete_password(id_input)
+
+                else:
+                    break
+
+    except Error as e:
+        print("Error while connecting to MySQL", e)
         
-        connection = mysql.connector.connect(host=config['HOST'],
-                                            database=config['BDD'],
-                                            user=config['USER'],
-                                            password=config['PASSWORD'])
-        
-        bdd_connection = Query_manager(connection)   
-        
-        print("Welcome to your password manager !")
+    finally:
+        if connection != None:
+            bdd_connection.close_connection()
 
-        while True:
-            user_input = input(
-                "\n*****************************************\n\t1 - List services passwords\n\t2 - Add a password\n\t3 - Update a record\n\t4 - Delete a record\n\tAny other key - exit\n*****************************************\n")
-            cursor = connection.cursor()
-            if user_input == '1':
-                bdd_connection.display_all_passwords()
-                
-                passId = input("Choose password to reveal : \t")
-                bdd_connection.display_password(passId)
-
-            elif user_input == '2':     
-                bdd_connection.add_password()
-
-            elif user_input == '3':
-                bdd_connection.display_all_passwords()
-                
-                id_input = int(input("Which one do you want to update ? \t"))               
-                bdd_connection.edit_password(id_input)
-
-            elif user_input == '4':
-                bdd_connection.display_all_passwords()
-                
-                id_input = int(input("Which record do you want to delete ? \t"))
-                bdd_connection.delete_password(id_input)
-
-            else:
-                break
-
-except Error as e:
-    print("Error while connecting to MySQL", e)
-    
-finally:
-    if connection != None:
-        bdd_connection.close_connection()
+if __name__ == '__main__':
+    main()
