702f8e05f8a3f660fba36cce0835d980ca05596a
Emereole Chinduji <60651716+LowUp@users.noreply.github.com>
2024-01-13 14:11:07 +0100

    Added encryptions for storing passwords

--- password_classes.py
+++ password_classes.py
@@ -1,17 +1,28 @@
 import mysql.connector
 from mysql.connector import Error
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 import random
+from cryptography.fernet import Fernet
+import glob
 
-@dataclass(frozen=True)
+@dataclass(frozen=False)
 class Query_manager:
     connection: mysql
+    key: bytes = field(init=False)
     
-    def __post_init_(self) -> None:
+    def __post_init__(self) -> None:
         # self.connection = mysql.connector.connect(host = self.host, database = self.database, user = self.user,password = self.password)
         if self.connection.is_connected():
             db_Info = self.connection.get_server_info()
             print("Connected to MySQL Server version ", db_Info)
+            if not glob.glob("logs/key.txt"):
+                print("Genereting key...")
+                self.key = Fernet.generate_key()
+                with open("logs/key.txt", "wb") as f:
+                    f.write(self.key)
+            else:
+                with open("logs/key.txt", "rb") as f:
+                    self.key = f.read()
         else:
             print("Error while connecting to MySQL", Error)
     
@@ -26,7 +37,7 @@ class Query_manager:
         cursor.close()
         
     def generate_password(self, length: int) -> str:
-        characters: str = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-=[]{}|;:<>,./?'
+        characters: str = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@%^&*_+|:"<>,.;?'
         password: str = ""
         random_string: str = ""
         for i in range(length):
@@ -45,6 +56,7 @@ class Query_manager:
         cursor.close()
     
     def display_password(self, id) -> None:
+        fernet = Fernet(self.key)
         cursor = self.connection.cursor()
         cursor.execute(f"select * from passwords where id = %s;", (id,))
         record = cursor.fetchall()
@@ -56,11 +68,12 @@ class Query_manager:
             print("| id | - | service | - | username | - | password |\n")
             for count, value in enumerate(record):
                 print(
-                    f"| {value[0]} | - | {value[1]} | - | {value[2]} | - | {value[3]} |\n")
+                    f"| {value[0]} | - | {value[1]} | - | {value[2]} | - | {fernet.decrypt(bytes(value[3], 'utf-8')).decode()} |\n")
                 return None
         cursor.close()
     
     def add_password(self) -> None:
+        fernet = Fernet(self.key)
         cursor = self.connection.cursor()
         service = input("Input service name : \t")
         identifiant = input("Input username : \t")
@@ -68,13 +81,14 @@ class Query_manager:
         mdp = mdp or self.generate_password(20)
         cursor.execute(
             f"insert into passwords (service, identifiant, mdp) values (%s, %s, %s);", 
-            (service, identifiant, mdp)
+            (service, identifiant, fernet.encrypt(mdp.encode()))
         )
         self.connection.commit()
         print("Password added")
         cursor.close()
     
     def edit_password(self, id) -> None:
+        fernet = Fernet(self.key)
         cursor = self.connection.cursor()
         cursor.execute(f"select * from passwords where id = %s;", (id,))
         record = cursor.fetchall()
@@ -84,10 +98,8 @@ class Query_manager:
             print("No password found")
             return None
         
-        for count, value in enumerate(record):
-            print("| id | - | service | - | username | - | password |\n")
-            print(
-            f"| {value[0]} | - | {value[1]} | - | {value[2]} | - | {value[3]} |\n")
+        # Display password
+        self.display_password(id)     
         
         print("Leave blank and press ENTER to not modify values\n")
         service_input = input("Update service name : \t")
@@ -97,6 +109,11 @@ class Query_manager:
         input_dict.update({"identifiant": identifiant_input})
 
         mdp_input = input("Update password : \t")
+        if mdp_input:
+            mdp_input = fernet.encrypt(mdp_input.encode())
+        else:
+            mdp_input = record[0][3]
+        
         input_dict.update({"mdp": mdp_input})
 
         sql_query = "update passwords set "
@@ -105,7 +122,12 @@ class Query_manager:
         for key, value in input_dict.items():
             if str(value).lower() != '':
                 if counter == 0:
-                    sql_query+=f"{key} = '{value}' "
+                    if key == "mdp":
+                        sql_query+=f"{key} = %s "
+                    else:
+                        sql_query+=f"{key} = '{value}' "
+                elif key == "mdp":
+                    sql_query+=f", {key} = %s "
                 else:
                     sql_query+=f", {key} = '{value}' "
                 counter += 1
@@ -114,7 +136,9 @@ class Query_manager:
         
         if sql_query != f"update passwords set where id = {id}":
             cursor.execute(
-                    sql_query)
+                    sql_query, 
+                    (mdp_input,)
+                )
             self.connection.commit()
             print("Record updated !")
         else:
